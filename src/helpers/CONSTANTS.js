f8b7f630ce9f6eee271ae8b610608a992f39788b
Alex Nou <alexnou@gmail.com>
2020-08-16 02:34:36 -0500

    npm audit fix

--- src/helpers/CONSTANTS.js
+++ src/helpers/CONSTANTS.js
@@ -1,2 +1,2 @@
-export let API_ENDPOINT = "https://bdocraftprofit-api.herokuapp.com/api"
-// export let API_ENDPOINT = "localhost:5000/api"
+export let API_ENDPOINT = process.env.NODE_ENV === 'production' ? "https://bdocraftprofit-api.herokuapp.com/api" : "localhost:5000/api"
+console.log("API ENDPOINT:", API_ENDPOINT)
